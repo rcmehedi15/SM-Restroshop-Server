80e412db6f80ec5e056a163ae36113957587131c
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-06-02 14:48:19 +0600

    user api fetch done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middle ware 
 app.use(cors());
@@ -27,21 +27,54 @@ async function run() {
         await client.connect();
 
         // server to connect mongodb database
+        const usersCollection = client.db("SMrestroDB").collection("users");
         const menuCollection = client.db("SMrestroDB").collection("Menu");
         const reviewsCollection = client.db("SMrestroDB").collection("reviews");
+        const cartCollection = client.db("SMrestroDB").collection("carts");
+
+
+        // user related api 
+        app.post('/users',async(req,res) =>{
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
         // all menu data receive http://localhost:5000/menu
-        app.get('/menu', async(req,res) => {
+        app.get('/menu', async (req, res) => {
             const result = await menuCollection.find().toArray()
             res.send(result)
         })
-        // all review data receive http://localhost:5000/review
+        // all review data receive http://localhost:5000/reviews
 
-        app.get('/reviews', async(req,res) => {
+        app.get('/reviews', async (req, res) => {
             const result = await reviewsCollection.find().toArray()
             res.send(result)
         })
+        // cart collection http://localhost:5000/carts
+        app.post('/carts/', async (req, res) => {
+            const item = req.body;
+            console.log(item);
+            const result = await cartCollection.insertOne(item);
+            res.send(result)
+        })
+        // unique data receive
+        app.get('/carts', async (req, res) => {
+            const email = req.query.email;
+            if (!email) {
+                res.send([]);
+            }
+            const query = { email: email };
+            const result = await cartCollection.find(query).toArray();
+            res.send(result);
 
+        })
 
+        app.delete('/carts/:id',async(req,res) =>{
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
         // Send a ping to confirm a successful connection
@@ -62,3 +95,16 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`SM RESTRO is siiting on port ${port}`);
 })
+/**
+ * ---------------------------------------
+ * Naming Convention
+ * --------------------------------------
+ * 1. users : userCollection
+ * 2. app.get('/users')    = all user get
+ * 3. app.get('/users/:id') = peticular user neeeded
+ * 4. app.post('/users') = new post create
+ * 5. app.patch('/users/:id') = perticular user data update
+ * 6. app.put('/users/:id')
+ * 7. app.delete('users/:id') = user delete or data delete
+ * 
+ */
