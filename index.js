c9d8e2a2508e0a035af6e42285291d430f1462e4
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-06-06 00:52:41 +0600

    payment mongodb store done

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ async function run() {
         const menuCollection = client.db("SMrestroDB").collection("Menu");
         const reviewsCollection = client.db("SMrestroDB").collection("reviews");
         const cartCollection = client.db("SMrestroDB").collection("carts");
+        const paymentCollection = client.db("SMrestroDB").collection("payments");
 
         app.post('/jwt', (req, res) => {
             const user = req.body;
@@ -209,6 +210,18 @@ async function run() {
             res.send({
                 clientSecret: paymentIntent.client_secret
             })
+        })
+
+
+        // payment realted api
+        app.post('/payments', verifyJWT, async (req, res) => {
+            const payment = req.body;
+            const insertResult = await paymentCollection.insertOne(payment);
+
+            const query = { _id: { $in: payment.cartItems.map(id => new ObjectId(id)) } }
+            const deleteResult = await cartCollection.deleteMany(query);
+
+            res.send({ insertResult, deleteResult })
         })
 
         // Send a ping to confirm a successful connection
